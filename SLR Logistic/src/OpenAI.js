240dedfa20e76e02e3796bd4699d77d001c64dd4
shivamgupta214 <31489905+shivamgupta214@users.noreply.github.com>
2024-01-16 14:25:28 -0600

    Update OpenAI.js

--- SLR Logistic/src/OpenAI.js
+++ SLR Logistic/src/OpenAI.js
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import '@chatscope/chat-ui-kit-styles/dist/default/styles.min.css';
 import { MainContainer, ChatContainer, MessageList, Message, MessageInput, TypingIndicator } from '@chatscope/chat-ui-kit-react';
 
-const API_KEY = "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";
+const API_KEY = "YourAPIKEy";
 // "Explain things like you would to a 10 year old learning how to code."
 const systemMessage = { //  Explain things like you're talking to a software professional with 5 years of experience.
   "role": "system", "content": "Explain things like you're talking to a software professional with 2 years of experience."
